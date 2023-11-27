50bfec1168f6e4a570d2ab7a60d5bbfdb3028dda
farihas15 <145361888+farihas15@users.noreply.github.com>
2023-11-26 19:48:55 -0500

    Add files via upload

---
+++ aboutme/abme.js
@@ -0,0 +1,8 @@
+document.getElementById('more-details').addEventListener('click', function() {
+    var details = document.getElementById('details');
+    if (details.style.display === 'none') {
+        details.style.display = 'block';
+    } else {
+        details.style.display = 'none';
+    }
+});// JavaScript Document
